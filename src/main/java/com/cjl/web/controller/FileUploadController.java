eb6b99fa68ac139e26f26c9c27c4c66e80845bf7
fuhuifang <18838928050@163.com>
2018-11-04 21:55:21 +0800

    上传文件做一半

--- src/main/java/com/cjl/web/controller/FileUploadController.java
+++ src/main/java/com/cjl/web/controller/FileUploadController.java
@@ -1,19 +1,13 @@
 /**
- * @copy right Stateally Interactive Company All rights reserved
- *
- * @Title: FileUploadController.java 
- *
- * @Date:  2018年11月2日  下午5:37:27
- *
- * @Package com.cjl.web.controller
+ * 
  */
-
-
 package com.cjl.web.controller;
 
 import java.io.File;
 
-import org.apache.commons.io.FilenameUtils;
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -22,45 +16,70 @@ import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 
 /**
- * @author 付慧芳
- * 
- * 日期：2018年11月2日 下午5:37:27
- *
- * 描述：
- * @version V1.0
- *
- */
-
+ @author： fu    @time：2018年11月4日 下午4:02:36 
+ @说明： 一份耕耘，一份收获
+**/
 @Controller
+@RequestMapping("/")
 public class FileUploadController {
 
-    private static String UPLOAD_DIRECTORY = PropertiesUtil.get("fileupload.directory", "");
+	//通过Spring的autowired注解获取spring默认配置的request
+		@Autowired
+		private HttpServletRequest request;
+		
+		@RequestMapping("toUpload")
+		public String toUpload() {
+			return "fileDetail/upload";
+			
+		}
+		/***
+		 * 上传文件 用@RequestParam注解来指定表单上的file为MultipartFile
+		 * 
+		 * @param file
+		 * @return
+		 */
+		@RequestMapping(value="fileUpload", method = RequestMethod.POST)
+		//required=false,可以上传两个文件
+		public String fileUpload(@RequestParam(value="file",required=false) MultipartFile file,HttpServletRequest request) {
+			// 判断文件是否为空
+			if (!file.isEmpty()) {
+				try {
+					//使用时间戳定义不重复的图片名称
+					String imgName=System.currentTimeMillis()+".png";
+					// 拿到项目环境地址 
+					String filePath = request.getSession().getServletContext().getRealPath("/") + "upload/";
+					File targetFile=new File(filePath,imgName);
+						if (!targetFile.exists()) {
+							targetFile.mkdirs();//创建文件夹
+							
+						}
+					//getOriginalFilename()拿到原生名称，使用imgName代替了，避免重复，但是只能在单线程使用
+					// 转存文件，把当前的文件拷贝到文件路径里面去
+					file.transferTo(new File(filePath));
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+			// 重定向
+			return "redirect:/list";
+		}
 
-    @RequestMapping(value = "uploadFile", method = RequestMethod.POST)
-    public ModelAndView uploadFile(@RequestParam("file") MultipartFile file){
-        // 判断文件是否为空
-        if (!file.isEmpty()) {
-            try {
-                //判断文件目录是否存在，否则自动生成
-                File directory = new File(UPLOAD_DIRECTORY);
-                if (!directory.exists()){
-                    directory.mkdirs();
-                }
 
-                //失败跳转视图
-                if (file.getSize() > 30000)
-               return new ModelAndView("uploadFail","msg",file.getOriginalFilename()+"超过了指定大小");
-
-                // 文件保存路径
-                String filePath =  FilenameUtils.concat(UPLOAD_DIRECTORY, file.getOriginalFilename());
-                // 转存文件
-                file.transferTo(new File(filePath));
-
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
-
-        //成功跳转视图
-        return new ModelAndView("uploadSuccess","msg",file.getOriginalFilename());
-    }
+		/***
+		 * 读取上传文件中得所有文件并返回
+		 * 
+		 * @return
+		 */
+		@RequestMapping(value="list", method = RequestMethod.POST)
+		public ModelAndView list() {
+			String filePath = request.getSession().getServletContext().getRealPath("/") + "upload/";
+			ModelAndView mav = new ModelAndView("list");
+			File uploadDest = new File(filePath);
+			String[] fileNames = uploadDest.list();
+			for (int i = 0; i < fileNames.length; i++) {
+				//打印出文件名
+				System.out.println(fileNames[i]);
+			}
+			return mav;
+		}
+}
--- src/main/webapp/WEB-INF/jsp/fileDetail/upload.jsp
+++ src/main/webapp/WEB-INF/jsp/fileDetail/upload.jsp
@@ -1,17 +1,21 @@
-<%@ page language="java" contentType="text/html; charset=UTF-8"
+<%@ page language="java" contentType="text/html;charset=UTF-8"
     pageEncoding="UTF-8"%>
-<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<%@taglib uri="http://www.springframework.org/tags/form" 
+prefix="form" %>
+<meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
 <title>this title</title>
 </head>
 <body>
-  <h2>文件上传实例</h2>
-    <form action="/uploadFile" method="post" enctype="multipart/form-data">
+<body>
+ 
+    <form action="fileUpload" method="post" enctype="multipart/form-data">
         选择文件:<input type="file" name="file">
         <input type="submit" value="提交">
     </form>
+</body>
 </body>
 </html>
